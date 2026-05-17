09226fbe6efde9f7bc0902af6720aa2500ab4f45
ronaldidro <rjdiazi10@gmail.com>
2026-05-17 08:40:26 -0500

    feat(users): add hashed password to update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '@users/dto/create-user.dto';
 import { UpdateUserDto } from '@users/dto/update-user.dto';
 import { User } from '@users/entities/user.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -15,9 +19,8 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const user = this.repository.create(createUserDto);
-    const salt = await bcrypt.genSalt();
 
-    user.password = await bcrypt.hash(createUserDto.password, salt);
+    user.password = await this.hashedPassword(createUserDto.password);
 
     return await this.repository.save(user);
   }
@@ -58,11 +61,27 @@ export class UsersService {
   async update(
     id: string,
     updateUserDto: UpdateUserDto,
-  ): Promise<UpdateResult> {
-    return await this.repository.update(id, updateUserDto);
+    currentUser: User,
+  ): Promise<User> {
+    if (!currentUser.isAdmin && id !== currentUser.id)
+      throw new ForbiddenException('Account invalid');
+
+    const user = await this.repository.preload({ id, ...updateUserDto });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    if (updateUserDto.password)
+      user.password = await this.hashedPassword(updateUserDto.password);
+
+    return await this.repository.save(user);
   }
 
   async remove(id: string): Promise<DeleteResult> {
     return await this.repository.softDelete(id);
+  }
+
+  private async hashedPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
   }
 }
