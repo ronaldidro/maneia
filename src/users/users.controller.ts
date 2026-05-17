1999a810a3f086db34e0059b7159873339b674cc
ronaldidro <rjdiazi10@gmail.com>
2026-05-17 08:41:25 -0500

    refactor(users): update users controller permissions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,13 +14,15 @@ import { UpdateUserDto } from '@users/dto/update-user.dto';
 import { Roles } from '@decorator/roles.decorator';
 import { Role } from '@app/base/role.enum';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { CurrentUser } from '@decorator/user.decorator';
+import { User } from '@users/entities/user.entity';
 
 @ApiBearerAuth()
-@Roles(Role.Admin)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
@@ -40,10 +42,12 @@ export class UsersController {
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateUserDto: UpdateUserDto,
+    @CurrentUser() currentUser: User,
   ) {
-    return this.usersService.update(id, updateUserDto);
+    return this.usersService.update(id, updateUserDto, currentUser);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.usersService.remove(id);
