0873fe9def648eab0d1e1a76e16a931fa2cd78d2
ronaldidro <rjdiazi10@gmail.com>
2026-06-26 18:09:53 -0500

    fix(mailer): add googleapis

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,30 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
 import { MailerService } from '@/mailer/mailer.service';
 import { MailerController } from './mailer.controller';
 
 @Module({
-  imports: [
-    NestMailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          // service: 'gmail',
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          auth: {
-            type: 'OAuth2',
-            user: config.get<string>('mailer.user'),
-            clientId: config.get<string>('mailer.client_id'),
-            clientSecret: config.get<string>('mailer.client_secret'),
-            refreshToken: config.get<string>('mailer.refresh_token'),
-          },
-        },
-      }),
-    }),
-  ],
   providers: [MailerService],
   controllers: [MailerController],
 })
--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,12 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import { MailerService as NestMailerService } from '@nestjs-modules/mailer';
 import { CreateMailerDto } from './dto/create-mailer.dto';
+import { gmail_v1, google } from 'googleapis';
+import MailComposer from 'nodemailer/lib/mail-composer';
 
 @Injectable()
 export class MailerService {
-  constructor(private readonly mailerService: NestMailerService) {}
+  private gmail: gmail_v1.Gmail;
+
+  constructor() {
+    const oauth2Client = new google.auth.OAuth2({
+      client_id: process.env.MAIL_CLIENT_ID,
+      client_secret: process.env.MAIL_CLIENT_SECRET,
+    });
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.MAIL_REFRESH_TOKEN,
+    });
+
+    this.gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+  }
 
   async sendMail(createMailerDto: CreateMailerDto) {
-    await this.mailerService.sendMail(createMailerDto);
+    try {
+      const mail = new MailComposer({
+        from: 'nobody@gmail.com',
+        textEncoding: 'base64',
+        ...createMailerDto,
+      });
+
+      const message = await mail.compile().build();
+
+      const raw = Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+
+      const result = await this.gmail.users.messages.send({
+        userId: 'me',
+        requestBody: { raw },
+      });
+
+      console.log('Email sent successfully:', result.data.id);
+      return result.data;
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw error;
+    }
   }
 }
