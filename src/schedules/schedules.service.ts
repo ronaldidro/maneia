12ea5a766c6ce6052020a233a64635bcdb8e93c6
ronaldidro <rjdiazi10@gmail.com>
2026-09-19 16:24:47 -0500

    refactor(schedules): update check limit time

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class SchedulesService {
   private readonly logger = new Logger(SchedulesService.name);
 
-  @Cron(CronExpression.EVERY_12_HOURS)
+  @Cron('0 9,21 * * *')
   checkLimit() {
-    this.logger.debug('Called every 30 minutes');
+    this.logger.debug('Called 9 and 21 hours');
   }
 }
